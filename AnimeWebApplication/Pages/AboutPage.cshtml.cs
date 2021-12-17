8f1df6ea888047e238e6c563bce24c1006fd7e10
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-12-17 23:44:13 +0300

    add about and rule page

---
+++ AnimeWebApplication/Pages/AboutPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AnimeWebApplication.Pages
+{
+    public class AboutPage : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
